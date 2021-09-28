70dcf21481a02cd3dac3ed951981708ebbbfb83d
Varad Lata Ratnakar Desai <varad.desai.rl@gmail.com>
2021-09-28 10:25:19 -0400

    Added comments to understand connection in between scripts

--- GitHelper/gitACP.sh
+++ GitHelper/gitACP.sh
@@ -1,4 +1,9 @@
-# Command to execute this script
-# gitACP.sh <branch name> <comment for commit>
+## Command to execute this script
+## gitACP.sh <branch name> <comment for commit>
 
 git add -A :/ && git commit -m "$2" && git push varad $1
+
+## After this script
+## Creat Pull Request and Merge Pull Request
+## To Organization -> Main
+## From Fork -> branch
